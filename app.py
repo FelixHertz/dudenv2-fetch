e671e642ac0de97031cc876dcb884eef667c02dc
FelixHertz <84127057+FelixHertz@users.noreply.github.com>
2022-10-25 16:42:58 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,29 +1,12 @@
 from flask import Flask, render_template, request, flash
-import time
-from datetime import datetime
-import base64
-import os.path
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import time
-import os
 import mysql.connector
 
-mydb = mysql.connector.connect(host="sql11.freesqldatabase.com", user="sql11524700", passwd="tY17NVeXtN", database="sql11524700")
+mydb = mysql.connector.connect(host="h1.host.filess.io", user="dudenv2_bystreamwe", passwd="239848e4ad651c7b21981c6cd1fea4bf6ec5c444", database="dudenv2_bystreamwe", port="3307")
 mycursor = mydb.cursor()
 
 def retrieve():
 	sql = "SELECT wort FROM wort_erg"
 	mycursor.execute(sql)
-	
-
-
-
 
 ## Create Flask App
 app = Flask(__name__)
@@ -37,8 +20,6 @@ app.secret_key = "manbearpig_MUDMAN888"
 def index():
     return render_template("index.html") 
    
-
-
 
 @app.route("/liste", methods=['POST', 'GET'])
 def greeter():
