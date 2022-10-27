be8a09fd0d77710a874f4b3865f48fe63ec61157
FelixHertz <84127057+FelixHertz@users.noreply.github.com>
2022-10-27 19:41:54 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ import mysql.connector
 mydb = mysql.connector.connect(host="h1.host.filess.io", user="dudenv2_bystreamwe", passwd="239848e4ad651c7b21981c6cd1fea4bf6ec5c444", database="dudenv2_bystreamwe", port="3307")
 mycursor = mydb.cursor()
 
+
 def retrieve():
 	sql = "SELECT wort FROM wort_erg"
 	mycursor.execute(sql)
+
 
 ## Create Flask App
 app = Flask(__name__)
@@ -18,13 +20,26 @@ app.secret_key = "manbearpig_MUDMAN888"
 
 @app.route("/")
 def index():
-    return render_template("index.html") 
+	retrieve()
+	latest = "Resuemieren"
+	for i in mycursor:
+		latest = i
+	flash(''.join(latest), 'category1')
+	return render_template("index.html")
    
 
 @app.route("/liste", methods=['POST', 'GET'])
 def greeter():
-	retrieve()
-	for i in mycursor:
-		flash(i)
-	return render_template("index.html")
+	cur = mydb.cursor()
+	cur.execute("SELECT * FROM wort_erg")
+	data = cur.fetchall()
+	return render_template("liste.html", data=data)
+
+
+@app.route("/alles", methods=['POST', 'GET'])
+def all():
+	cur = mydb.cursor()
+	cur.execute("SELECT * FROM other")
+	data = cur.fetchall()
+	return render_template("liste.html", data=data)
 
